67e15f8cafe33cead14665c971f740ceccfc042e
IremK <ayseirem.kayacioglu@gmail.com>
2025-06-05 23:40:59 +0300

    Add ActionCard.cs

--- Assets/Scripts/ActionCard.cs
+++ Assets/Scripts/ActionCard.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ActionCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string cardTitle;
+    public string description;
+    public int ppChange, apChange, scChange;
+
+    public TMP_Text titleText;
+    public TMP_Text descText;
+
+    public void Setup(string title, string desc, int pp, int ap, int sc)
     {
-        
+        cardTitle = title;
+        description = desc;
+        ppChange = pp;
+        apChange = ap;
+        scChange = sc;
+        titleText.text = title;
+        descText.text = desc;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayCard()
     {
-        
+        GameManager.Instance.ApplyScoreChanges(ppChange, apChange, scChange);
+        Destroy(gameObject); // Optional: remove after playing
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     public int publicPerception = 50;
     public int activistPressure = 50;
     public int shareholderConfidence = 50;
 
     public Slider ppSlider, apSlider, scSlider;
+    
+    void Awake() {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
 
     void Update()
     {
@@ -27,4 +34,12 @@ public class GameManager : MonoBehaviour
             // TODO: Trigger Game Over screen
         }
     }
+
+    public void ApplyScoreChanges(int ppChange, int apChange, int scChange)
+    {
+        publicPerception += ppChange;
+        activistPressure += apChange;
+        shareholderConfidence += scChange;
+    }
+
 }
