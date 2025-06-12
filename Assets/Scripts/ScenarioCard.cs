28c9ce617f40b3093318cf95474ebf6b67a9c885
I-rem <ayseirem.kayacioglu@gmail.com>
2025-06-12 13:19:25 +0300

    Add scriptable objects, little work on tutorial buttons

---
+++ Assets/Scripts/ScenarioCard.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScenario", menuName = "PRGame/Scenario Card")]
+public class ScenarioCard : ScriptableObject
+{
+    public string scenarioTitle;
+
+    [TextArea(2, 5)]
+    public string scenarioText;
+    public Choice[] choices;
+}
+
+[System.Serializable]
+public class Choice
+{
+    public string choiceText;
+    public int publicPerception;
+    public int activistPressure;
+    public int stakeholderSupport;
+    public int farmersProducers;
+    public int governmentRelations;
+    public int money;
+    public ScenarioCard followUpScenario; // for chaining
+}
